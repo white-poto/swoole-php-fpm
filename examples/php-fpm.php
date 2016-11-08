501364ee8691399409085499bcccc21114a6c346
huyanping <hypxm@qq.com>
2016-11-08 16:55:19 +0800

    tests

--- examples/php-fpm.php
+++ examples/php-fpm.php
@@ -11,7 +11,7 @@ require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR 
 $server = new swoole_server('127.0.0.1', 9001);
 $server->set(array(
     'reactor_num' => 2, //reactor thread num
-    'worker_num' => 4,    //worker process num
+    'worker_num' => 30,    //worker process num
     'backlog' => 128,   //listen backlog
     'max_request' => 50,
     'dispatch_mode' => 2,
