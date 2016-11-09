3a7328366bfb95eadbf9b125817bccaf8745be9e
huyanping <hypxm@qq.com>
2016-11-09 09:12:35 +0800

    tests

--- src/Handler/EchoHandler.php
+++ src/Handler/EchoHandler.php
@@ -18,7 +18,7 @@ class EchoHandler implements HandlerInterface
         var_dump($record);
         $body = file_get_contents("http://qidian.qpic.cn/qidian_common/349573/c6307e16b36ee882c844f38103628c65/0");
         $length = strlen($body);
-        $data = "Status: 200\r\nServer: X2S_Platform\r\nConnection: keep-alive\r\nDate: Tue, 08 Nov 2016 08:22:43 GMT\r\nCache-Control: max-age=2592000\r\nExpires: Thu, 08 Dec 2016 08:22:43 GMT\r\nLast-Modified: Mon, 07 Nov 2016 11:35:21 GMT\r\nContent-Type: image/jpeg\r\nContent-Length: {$length}\r\nKeep-Alive: timeout=60\r\nX-Cache-Lookup: Hit From Disktank\r\n\r\n";
+        $data = "HTTP/1.1 200 OK\r\nServer: X2S_Platform\r\nConnection: keep-alive\r\nDate: Tue, 08 Nov 2016 08:22:43 GMT\r\nCache-Control: max-age=2592000\r\nExpires: Thu, 08 Dec 2016 08:22:43 GMT\r\nLast-Modified: Mon, 07 Nov 2016 11:35:21 GMT\r\nContent-Type: image/jpeg\r\nContent-Length: {$length}\r\nKeep-Alive: timeout=60\r\nX-Cache-Lookup: Hit From Disktank\r\n\r\n";
         $data .= $body;
 
         $messages = array(
